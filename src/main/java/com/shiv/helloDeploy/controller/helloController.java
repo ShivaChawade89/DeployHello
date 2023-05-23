7398c843125206d0f4aec750c4034225b2052a2d
shiv kumar chawade <shvachawade89@mail.com>
2023-05-23 17:48:52 +0530

    first commit

---
+++ src/main/java/com/shiv/helloDeploy/controller/helloController.java
@@ -0,0 +1,12 @@
+package com.shiv.helloDeploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+    @GetMapping("get")
+    public String hello(){
+        return "hello User";
+    }
+}
---
+++ src/test/java/com/shiv/helloDeploy/HelloDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiv.helloDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
